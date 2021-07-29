62e07bb2cbae469133f70b9d28e643e3f8a08a66
zhenquan.qiu <gnsyxiang@163.com>
2021-07-29 08:49:20 +0800

    feat: add test demo for client and server

---
+++ example/server.c
@@ -0,0 +1,84 @@
+/**
+ * 
+ * Release under GPLv-3.0.
+ * 
+ * @file    server.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    18/03 2021 20:22
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        18/03 2021      create the file
+ * 
+ *     last modified: 18/03 2021 20:22
+ */
+#include <stdio.h>
+
+#include "module_a.h"
+
+#include "hy_utils/hy_module.h"
+#include "hy_utils/hy_mem.h"
+#include "hy_utils/hy_log.h"
+
+#define ALONE_DEBUG 1
+
+typedef struct {
+    void *log_handle;
+} _main_context_t;
+
+static void _module_destroy(_main_context_t **context_pp)
+{
+    _main_context_t *context = *context_pp;
+
+    // note: 增加或删除要同步到module_create_t中
+    module_destroy_t module[] = {
+        {"log",     &context->log_handle,   HyLogDestroy},
+    };
+
+    RUN_DESTROY(module);
+
+    HY_FREE(context_pp);
+}
+
+static _main_context_t *_module_create(void)
+{
+    _main_context_t *context = malloc(sizeof(*context));
+    if (!context) {
+        LOGE("malloc faild \n");
+        return NULL;
+    }
+    memset(context, '\0', sizeof(*context));
+
+    HyLogConfig_t log_config;
+    log_config.buf_len = 512;
+    log_config.level = HY_LOG_LEVEL_INFO;
+    log_config.config_file = "./res/config/log4cplus.rc";
+
+    // note: 增加或删除要同步到module_destroy_t中
+    module_create_t module[] = {
+        {"log",  &context->log_handle,   &log_config,    (create_t)HyLogCreate,    HyLogDestroy},
+    };
+
+    RUN_CREATE(module);
+
+    return context;
+}
+
+
+int main(int argc, char const* argv[])
+{
+    _main_context_t *context = _module_create();
+    if (!context) {
+        LOGE("_module_create faild \n");
+        return -1;
+    }
+
+    _module_destroy(&context);
+
+    return 0;
+}
